4c9eb49114cbf9e5303a115d3f72af97f0d4f9a5
jkotov <jkotov@jkotov.com>
2023-03-25 16:17:03 +0300

    override for quest status dialog block text

--- Assets/Scripts/Dialog/DialogBlock.cs
+++ Assets/Scripts/Dialog/DialogBlock.cs
@@ -2,18 +2,32 @@ using System;
 using System.Collections.Generic;
 using QuestSystem;
 using UnityEngine;
+using System.Linq;
 
 namespace Dialog
 {
     [CreateAssetMenu(fileName = "New Dialog Block", menuName = "ScriptableObjects/Dialog Block")]
     public class DialogBlock : ScriptableObject
     {
-        public string blockText;
+        public string BlockText
+        { 
+            get
+            {
+                foreach (var text in overridedText)
+                {
+                    if (text.quest.questStatus == text.status)
+                        return text.blockText;
+                }
+                return blockText;
+            }
+        }
+        [SerializeField] private string blockText;
         public List<Actor> actors;
         public List<Answer> Answers => answers.FindAll(answer => answer.IsActive);
         [SerializeField] private List<Answer> answers;
         [SerializeField] private List<QuestStatusChanger> statusChangers;
         [SerializeField] private List<InventoryDialogAction> inventoryActions;
+        [SerializeField] private List<TextForStatus> overridedText;
 
         public void DoActions()
         {
@@ -28,6 +42,14 @@ namespace Dialog
                 inventoryAction.ChangeInventoryItemsCount();
             }
         }
+        
+        [Serializable]
+        public struct TextForStatus
+        {
+            [SerializeField] public string blockText;
+            [SerializeField] public Quest quest;
+            [SerializeField] public QuestStatus status;
+        }
     }
     
     [Serializable]
@@ -40,4 +62,5 @@ namespace Dialog
         public bool IsActive => questConditions.TrueForAll(condition => condition.Completed) &&
                                 itemConditions.TrueForAll(condition => condition.Completed);
     }
+
 }
